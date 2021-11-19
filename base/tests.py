d5f9a5adbb749dd57238b69798ebb29c795005fd
Marcos Turossi <marcosturossi@gmail.com>
2021-11-19 15:00:12 -0300

    Iniciando a melhoria na programação orientada a objeto

--- base/tests.py
+++ base/tests.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
 from .language.c import C
+from .supervisors.supervisor import Supervisor
 
 
 class TesteC(TestCase):
     def setUp(self):
         self.c = C()
+        self.supervisor = Supervisor(C()) # Test Supervisor Modular Local
 
-    def testa_declara_variavel(self):
+    def testa_languague_sintax(self):
         function_exemple ='int main(void){\n    println("Hello World");\n}\n'
         self.assertEqual(self.c.o_declare_var('int', 'main', 3), "int main = 3;\n")
         self.assertEqual(self.c.o_declare_function('int', 'main', 'println("Hello World")'), function_exemple)
+
+    def testa_modular_local(self):
+        pass
--- base/views.py
+++ base/views.py
@@ -2,7 +2,6 @@ from django.urls import reverse_lazy
 from django.views.generic import FormView
 from django.http import HttpResponse
 from django.shortcuts import redirect
-
 from .forms import InputForm, AutomataFormset
 from .reader import clean_data
 from .supervisors.localization import SupervisorLocalizado
@@ -44,10 +43,14 @@ class Home(FormView):
                     supervisor.set_data(data_sup)
                 self.request.session['code'] = supervisor.createcode_c()
             else:
-                if form.cleaned_data['linguagem'] == "C":
-                    supervisor = SupervisorLocalizado(C())
-                else:
-                    supervisor = SupervisorLocalizado(Arduino())
-                pass # TODO, falta implantar a modularização com diversos arquivos
+                self.request.session['code'] = ""
+                for i in formset:
+                    data_sup = {'plant': clean_data(i['planta']), 'supervisor': clean_data(i['supervisor'])}
+                    if form.cleaned_data['linguagem'] == "C":
+                        supervisor = SupervisorLocalizado(C())
+                    else:
+                        supervisor = SupervisorLocalizado(Arduino())
+                    supervisor.set_data(data_sup)
+                    self.request.session['code'] += supervisor.createcode_c()
             return redirect('base:file')
         return super().form_valid(form)
